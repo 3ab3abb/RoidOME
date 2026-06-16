c7e96012aa9cc58e3a702f3917ddee8e25c36efd
3ab3abb <149205424+3ab3abb@users.noreply.github.com>
2026-06-16 14:50:12 +0100

    chore: remove PlatformIO build artifacts from repo

--- firmware/esp32_cam/.pio/build/esp32cam/FrameworkArduino/esp32-hal-tinyusb.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32cam/FrameworkArduino/esp32-hal-tinyusb.c.o: \
- /Users/kaisabaab/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-tinyusb.c \
- /Users/kaisabaab/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
